2bef917b32ed3cf88a7f6640761c08093ba7423e
Werner Robitza <werner.robitza@gmail.com>
2020-07-13 08:56:36 +0200

    update test values, add almost equal comparison

--- test/test.py
+++ test/test.py
@@ -17,11 +17,17 @@ REF = os.path.join(os.path.dirname(__file__), "ref-1280x720.mkv")
 class TestMetrics(unittest.TestCase):
     def test_ssim(self):
         run_ret = calc_ssim_psnr(DIST, REF)["ssim"]
-        self.assertEqual(run_ret, [{'n': 1, 'ssim_y': 0.936, 'ssim_u': 0.966, 'ssim_v': 0.951, 'ssim_avg': 0.951}, {'n': 2, 'ssim_y': 0.936, 'ssim_u': 0.966, 'ssim_v': 0.952, 'ssim_avg': 0.951}, {'n': 3, 'ssim_y': 0.935, 'ssim_u': 0.965, 'ssim_v': 0.952, 'ssim_avg': 0.951}])
+        expected = [{'n': 1, 'ssim_y': 0.936, 'ssim_u': 0.966, 'ssim_v': 0.951, 'ssim_avg': 0.951}, {'n': 2, 'ssim_y': 0.936, 'ssim_u': 0.966, 'ssim_v': 0.952, 'ssim_avg': 0.951}, {'n': 3, 'ssim_y': 0.935, 'ssim_u': 0.965, 'ssim_v': 0.952, 'ssim_avg': 0.951}]
+        for expected_frame, actual_frame in zip(expected, run_ret):
+            for key in ['ssim_y', 'ssim_u', 'ssim_v']:
+                self.assertAlmostEqual(expected_frame[key], actual_frame[key], places=2)
 
     def test_psnr(self):
         run_ret = calc_ssim_psnr(DIST, REF)["psnr"]
-        self.assertEqual(run_ret, [{'n': 1, 'mse_avg': 521.55, 'mse_y': 877.33, 'mse_u': 230.17, 'mse_v': 457.16, 'psnr_avg': 20.96, 'psnr_y': 18.7, 'psnr_u': 24.51, 'psnr_v': 21.53}, {'n': 2, 'mse_avg': 520.11, 'mse_y': 873.89, 'mse_u': 235.14, 'mse_v': 451.29, 'psnr_avg': 20.97, 'psnr_y': 18.72, 'psnr_u': 24.42, 'psnr_v': 21.59}, {'n': 3, 'mse_avg': 519.86, 'mse_y': 871.65, 'mse_u': 241.66, 'mse_v': 446.28, 'psnr_avg': 20.97, 'psnr_y': 18.73, 'psnr_u': 24.3, 'psnr_v': 21.63}])
+        expected = [{"n": 1, "mse_avg": 527.49, "mse_y": 890.56, "mse_u": 229.48, "mse_v": 462.44, "psnr_avg": 20.91, "psnr_y": 18.63, "psnr_u": 24.52, "psnr_v": 21.48}, {"n": 2, "mse_avg": 526.02, "mse_y": 887.23, "mse_u": 234.41, "mse_v": 456.41, "psnr_avg": 20.92, "psnr_y": 18.65, "psnr_u": 24.43, "psnr_v": 21.54}, {"n": 3, "mse_avg": 525.82, "mse_y": 885.0, "mse_u": 240.96, "mse_v": 451.48, "psnr_avg": 20.92, "psnr_y": 18.66, "psnr_u": 24.31, "psnr_v": 21.58}]
+        for expected_frame, actual_frame in zip(expected, run_ret):
+            for key in ['mse_avg', 'mse_y', 'mse_u', 'mse_v', 'psnr_avg', 'psnr_y', 'psnr_u', 'psnr_v']:
+                self.assertAlmostEqual(expected_frame[key], actual_frame[key], places=2)
 
 
 if __name__ == '__main__':
