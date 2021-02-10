e856af2282c45519a1c23a29dad1c27cc48baa55
Werner Robitza <werner.robitza@gmail.com>
2021-02-10 15:53:14 +0100

    improve help output from docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -2,12 +2,20 @@
 #
 # Helper script to run Docker container
 
+py_usage() {
+    docker run \
+    -t ffmpeg_quality_metrics \
+    python3 -m ffmpeg_quality_metrics -h
+}
+
 usage() {
     echo "Usage: $0 <dist> <ref> [OPTIONS]"
     echo
     echo "  <dist>    -- distorted video"
     echo "  <ref>     -- reference video"
-    echo "  [OPTIONS] -- further options passed to ffmpeg_quality_metrics.py"
+    echo "  [OPTIONS] -- further options passed to ffmpeg_quality_metrics.py, see below"
+    echo
+    py_usage
     exit 1
 }
 
