c602791c3edee136c066d61f7008e0ca3985ac6b
Werner Robitza <werner.robitza@gmail.com>
2021-03-06 12:57:05 +0100

    add VMAF unit test

--- test/test.py
+++ test/test.py
@@ -29,6 +29,13 @@ class TestMetrics(unittest.TestCase):
             for key in ['mse_avg', 'mse_y', 'mse_u', 'mse_v', 'psnr_avg', 'psnr_y', 'psnr_u', 'psnr_v']:
                 self.assertAlmostEqual(expected_frame[key], actual_frame[key], places=2)
 
+    def test_vmaf(self):
+        run_ret = ffqm(REF, DIST).calc_vmaf()
+        expected = [{'psnr': 18.587308, 'integer_motion2': 0.0, 'integer_motion': 0.0, 'ssim': 0.925976, 'integer_adm2': 0.69907, 'integer_adm_scale0': 0.708183, 'integer_adm_scale1': 0.733469, 'integer_adm_scale2': 0.718624, 'integer_adm_scale3': 0.67301, 'integer_vif_scale0': 0.539591, 'integer_vif_scale1': 0.718022, 'integer_vif_scale2': 0.751875, 'integer_vif_scale3': 0.773503, 'ms_ssim': 0.898265, 'vmaf': 14.054853, 'n': 1}, {'psnr': 18.60299, 'integer_motion2': 0.359752, 'integer_motion': 0.368929, 'ssim': 0.926521, 'integer_adm2': 0.698451, 'integer_adm_scale0': 0.706706, 'integer_adm_scale1': 0.73203, 'integer_adm_scale2': 0.718262, 'integer_adm_scale3': 0.672766, 'integer_vif_scale0': 0.540231, 'integer_vif_scale1': 0.719566, 'integer_vif_scale2': 0.753567, 'integer_vif_scale3': 0.775864, 'ms_ssim': 0.899353, 'vmaf': 14.464182, 'n': 2}, {'psnr': 18.613101, 'integer_motion2': 0.359752, 'integer_motion': 0.359752, 'ssim': 0.926481, 'integer_adm2': 0.697126, 'integer_adm_scale0': 0.706542, 'integer_adm_scale1': 0.731351, 'integer_adm_scale2': 0.716454, 'integer_adm_scale3': 0.671197, 'integer_vif_scale0': 0.539091, 'integer_vif_scale1': 0.718657, 'integer_vif_scale2': 0.753306, 'integer_vif_scale3': 0.775984, 'ms_ssim': 0.900086, 'vmaf': 14.256442, 'n': 3}]
+        for expected_frame, actual_frame in zip(expected, run_ret):
+            for key in expected_frame.keys():
+                self.assertAlmostEqual(expected_frame[key], actual_frame[key], places=2)
+
     def test_global(self):
         f = ffqm(REF, DIST)
         f.calc_ssim_psnr()
