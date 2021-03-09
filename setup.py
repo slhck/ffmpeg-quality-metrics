0ec4c3aa62a401b057b36a8226be22d21d924af7
Werner Robitza <werner.robitza@gmail.com>
2021-03-09 21:28:20 +0100

    support Python 3.9

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     packages=["ffmpeg_quality_metrics"],
     include_package_data=True,
