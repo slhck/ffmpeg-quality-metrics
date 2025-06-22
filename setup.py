4af27070e25af136fb1f2b6a8406254a3801226a
Werner Robitza <werner.robitza@gmail.com>
2025-06-22 20:27:01 +0200

    fix classifier

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Topic :: Multimedia :: Video",
-        "MIT",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
